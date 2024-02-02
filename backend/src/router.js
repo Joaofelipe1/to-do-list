c07d3f4812cfbe085d96f8e558b12b7cc1e041fc
unknown <felipeveigacampos@gmail.com>
2024-02-02 11:45:16 -0300

    first commit

---
+++ backend/src/router.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const router = express.Router();
+
+const taskController = require('./controllers/tasksController');
+const tasksMiddleware = require('./middlewares/tasksMiddleware');
+
+
+router.get('/tasks', taskController.getAll);
+router.post('/tasks',tasksMiddleware.validateFieldTitle, taskController.createTask);
+router.delete('/tasks/:id', taskController.deleteTask);
+router.put('/tasks/:id',
+    tasksMiddleware.validateFieldTitle,
+    tasksMiddleware.validateFieldStatus,
+    taskController.updateTask
+);
+
+module.exports = router;
